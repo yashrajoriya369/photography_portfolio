b529ce6c04e2a4b3c2849f2f1358b872625a06d3
Yash Rajoriya <yashrajoriya369@gmail.com>
2025-12-10 22:30:54 +0530

    update whyus page

--- src/component/Whyus.jsx
+++ src/component/Whyus.jsx
@@ -1,18 +1,7 @@
-import img1 from "../assets/img1.jpg";
-import img2 from "../assets/img2.jpg";
-import img3 from "../assets/img3.jpg";
-import img4 from "../assets/img4.jpg";
-import img5 from "../assets/img5.jpg";
-import img6 from "../assets/img6.jpg";
-import img7 from "../assets/img7.jpg";
-import img8 from "../assets/img8.jpg";
-import img9 from "../assets/img9.jpg";
-import img10 from "../assets/img10.jpg";
-import img11 from "../assets/img11.jpg";
-import img12 from "../assets/img12.jpg";
-import img13 from "../assets/img13.jpg";
-import img14 from "../assets/img14.jpg";
-import img15 from "../assets/img15.jpg";
+import img2 from "/photos/img2.jpg";
+import img4 from "/photos/img4.jpg";
+import img5 from "/photos/img5.jpg";
+import img8 from "/photos/img8.jpg";
 
 const WhyUs = () => {
   const cards = [
